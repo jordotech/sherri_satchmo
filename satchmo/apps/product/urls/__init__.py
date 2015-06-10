a4f3a27218037819c346d293d2e4b50dbe8df2a1
jordotech <jordotech@gmail.com>
2015-06-10 13:06:47 -0500

    use solid_i18n_patterns for product, category urls

--- satchmo/apps/product/urls/__init__.py
+++ satchmo/apps/product/urls/__init__.py
@@ -4,11 +4,12 @@ from products import urlpatterns as prodpatterns
 import product
 from satchmo_utils.signals import collect_urls
 from satchmo_store.shop import get_satchmo_setting
+from solid_i18n.urls import solid_i18n_patterns
 
 catbase = r'^' + get_satchmo_setting('CATEGORY_SLUG') + '/'
 prodbase = r'^' + get_satchmo_setting('PRODUCT_SLUG') + '/'
 
-urlpatterns = patterns('',
+urlpatterns = solid_i18n_patterns('',
     (prodbase, include('product.urls.products')),
     (catbase, include('product.urls.category')),
 )
